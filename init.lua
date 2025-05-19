1b71e6b3c44a089e9e7910f19c7827498410d970
Christian Solano <christiansolano@Christians-MacBook-Air-3.local>
2025-05-18 22:52:41 -0700

    Fix C-{j/k} keybindings

--- init.lua
+++ init.lua
@@ -287,8 +287,6 @@ vim.keymap.set('n', '<C-m>', '<cmd>Copilot disable<CR>')
 vim.keymap.set('n', '<C-c>', '<cmd>Copilot enable<CR>')
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 
-vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
-vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
 vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 
