62be1e4a904c1250f634180bbc213c1011806708
Christian Solano <christiansolano@f2af93eb-eb16-4853-8a7e-0a241211885d.attlocal.net>
2025-05-23 17:52:42 -0700

    Update file browser for creation

--- lua/custom/plugins/filebrowser.lua
+++ lua/custom/plugins/filebrowser.lua
@@ -1,6 +1,30 @@
 return {
   {
     'nvim-telescope/telescope-file-browser.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      local telescope = require 'telescope'
+      local fb_actions = require('telescope').extensions.file_browser.actions
+
+      telescope.setup {
+        extensions = {
+          file_browser = {
+            mappings = {
+              ['n'] = {
+                ['<C-c'] = fb_actions.create, -- custom create key
+              },
+              ['i'] = {
+                ['<C-c>'] = fb_actions.create,
+              },
+            },
+          },
+        },
+      }
+
+      telescope.load_extension 'file_browser'
+    end,
   },
 }
